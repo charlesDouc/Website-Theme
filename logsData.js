34a666b62698bafa2087f1b3644e940be929d39c
Charles Doucet <charles.douc@gmail.com>
2019-02-23 13:44:22 -0500

    Integrating the logs system - Pase A

--- logsData.js
+++ logsData.js
@@ -1,13 +1,11 @@
-<script type="text/javascript" src="https://code.jquery.com/jquery-1.12.0.min.js">
 
-	var spreadsheetId = "1UiQ5r59PSX1q9ylTstH5EFEu0fEHWTSkqsVHBmB_uVI",
-		url = "https://spreadsheets.google.com/feeds/list/" + spreadsheetId + "/od6/public/basic?alt=json";
+// Gets the id of the google spreadsheat
+var spreadsheetId = "1UiQ5r59PSX1q9ylTstH5EFEu0fEHWTSkqsVHBmB_uVI",
+	url = "https://spreadsheets.google.com/feeds/list/" + spreadsheetId + "/od6/public/basic?alt=json";
 
-	$.get({
-		url: url,
-		success: function(response) {
-			console.log(response);
-		}
-	});
-
-</script>
+$.get({
+	url: url,
+	success: function(response) {
+		console.log(response);
+	}
+});
